9855880b6830561110716f74a4ead450735e4f4f
hexWars <tech@sehnsucht.top>
2024-01-31 23:10:33 +0800

    style: change dark mode buttom style

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,52 +1,55 @@
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { Moon, Sun, SunMoon } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-    const [theme, setThemeState] = React.useState<
-        "theme-light" | "dark" | "system"
-    >("theme-light")
+    const [theme, setThemeState] = React.useState<"theme-light" | "dark" | "system">("theme-light")
 
     React.useEffect(() => {
         const isDarkMode = document.documentElement.classList.contains("dark")
         setThemeState(isDarkMode ? "dark" : "theme-light")
     }, [])
 
+    const toggleTheme = () => {
+        setThemeState(prevTheme => {
+            if (prevTheme === "theme-light") {
+                return "dark"
+            } else if (prevTheme === "dark") {
+                return "system"
+            } else {
+                return "theme-light"
+            }
+        })
+    }
+
     React.useEffect(() => {
         const isDark =
             theme === "dark" ||
-            (theme === "system" &&
-                window.matchMedia("(prefers-color-scheme: dark)").matches)
+            (theme === "system" && window.matchMedia("(prefers-color-scheme: dark)").matches)
         document.documentElement.classList[isDark ? "add" : "remove"]("dark")
     }, [theme])
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" onClick={toggleTheme}>
+            {theme === "theme-light" && (
+                <>
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setThemeState("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setThemeState("system")}>
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+                    <span className="sr-only">Toggle to dark mode</span>
+                </>
+            )}
+            {theme === "dark" && (
+                <>
+                    <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle to light mode</span>
+                </>
+            )}
+            {theme === "system" && (
+                <>
+                    <SunMoon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle to system mode</span>
+                </>
+            )}
+        </Button>
     )
 }
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -62,3 +62,4 @@ export function Navigation() {
         </NavigationMenu>
     );
 }
+
