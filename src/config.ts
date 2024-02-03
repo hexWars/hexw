a57a2f66c34a291747d7226d1859a06d2368de87
hexWars <tech@sehnsucht.top>
2024-02-03 12:46:40 +0800

    feat: add types.ts and config.ts

---
+++ src/config.ts
@@ -0,0 +1,136 @@
+import type { Site, SocialObject } from "@/types";
+
+export const SITE: Site = {
+    url: "https://blog.sehnsucht.top/",
+    author: "hexWers",
+    describe: "A minimal, responsive and SEO-friendly Astro blog theme.",
+    title: "Sehnsucht",
+    lightAndDarkMode: true,
+};
+
+export const LOCALE = {
+    lang: "zh",
+} as const;
+
+export const SOCIALS: SocialObject = [
+    {
+        name: "Github",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: ` ${SITE.title} on Github`,
+        active: true,
+    },
+    {
+        name: "Discord",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Discord`,
+        active: true,
+    },
+    {
+        name: "Mail",
+        href: "mailto:yourmail@gmail.com",
+        linkTitle: `Send an email to ${SITE.title}`,
+        active: true,
+    },
+    {
+        name: "Facebook",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Facebook`,
+        active: false,
+    },
+    {
+        name: "Instagram",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Instagram`,
+        active: false,
+    },
+    {
+        name: "LinkedIn",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on LinkedIn`,
+        active: false,
+    },
+    {
+        name: "Twitter",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Twitter`,
+        active: false,
+    },
+    {
+        name: "Twitch",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Twitch`,
+        active: false,
+    },
+    {
+        name: "YouTube",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on YouTube`,
+        active: false,
+    },
+    {
+        name: "WhatsApp",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on WhatsApp`,
+        active: false,
+    },
+    {
+        name: "Snapchat",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Snapchat`,
+        active: false,
+    },
+    {
+        name: "Pinterest",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Pinterest`,
+        active: false,
+    },
+    {
+        name: "TikTok",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on TikTok`,
+        active: false,
+    },
+    {
+        name: "CodePen",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on CodePen`,
+        active: false,
+    },
+    {
+        name: "GitLab",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on GitLab`,
+        active: false,
+    },
+    {
+        name: "Reddit",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Reddit`,
+        active: false,
+    },
+    {
+        name: "Skype",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Skype`,
+        active: false,
+    },
+    {
+        name: "Steam",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Steam`,
+        active: false,
+    },
+    {
+        name: "Telegram",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Telegram`,
+        active: false,
+    },
+    {
+        name: "Mastodon",
+        href: "https://github.com/hexWars/hexw",
+        linkTitle: `${SITE.title} on Mastodon`,
+        active: false,
+    },
+];
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+import type socialIcons from "@/assets/socialIcons";
+
+export type Site = {
+    url: string;
+    author: string;
+    describe: string;
+    title: string;
+    lightAndDarkMode: boolean;
+};
+
+export type SocialObject = {
+    name: keyof typeof socialIcons;
+    href: string;
+    active: boolean;
+    linkTitle: string;
+}[];
