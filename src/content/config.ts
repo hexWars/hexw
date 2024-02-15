5e79e47bdd65731b7edba5d4ead0344d9dc658d6
hexWars <tech@sehnsucht.top>
2024-02-15 21:51:32 +0800

    style: change some style

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,6 @@ const postsCollection = defineCollection({
         title: z.string(),
         pubDate: z.date(),
         description: z.string(),
-        author: z.string(),
         image: z.object({
             url: z.string(),
             alt: z.string()
