3f9e37ee70acfbe2cbc955c90eae94e686b43e88
hexWars <tech@sehnsucht.top>
2024-02-16 19:48:06 +0800

    feat: add page sort

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,6 @@ const postsCollection = defineCollection({
     schema: z.object({
         title: z.string(),
         pubDate: z.date(),
-        description: z.string(),
         image: z.object({
             url: z.string(),
             alt: z.string()
