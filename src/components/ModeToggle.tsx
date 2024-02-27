c739fbffcfd9f3a3d9a0353f03495cda7dbcab2b
hexWars <tech@sehnsucht.top>
2024-02-27 23:17:45 +0800

    style: change toggle style

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -31,7 +31,7 @@ export function ModeToggle() {
     }, [theme])
 
     return (
-        <Button variant="outline" size="icon" onClick={toggleTheme} className="bg-transparent hover:bg-transparent animate-pulse">
+        <Button variant="outline" size="icon" onClick={toggleTheme} className="bg-transparent hover:bg-transparent animate-pulse hover:animate-none">
             {theme === "theme-light" && (
                 <>
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 hover:stroke-ring" />
